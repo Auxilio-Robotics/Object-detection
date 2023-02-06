b3dc648d9b4b000cda0e3cccc507e9a6a4099287
pveenvenkatesh@gmail.com <pveenvenkatesh@gmail.com>
2023-02-06 18:09:19 -0500

    some changes

--- scripts/object_detection_node.py
+++ scripts/object_detection_node.py
@@ -13,11 +13,45 @@ from cv_bridge import CvBridge, CvBridgeError
 import message_filters
 from rospy.numpy_msg import numpy_msg
 import json
+import base64
+
+
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
+
+
+def json_numpy_obj_hook(dct):
+    """
+    Decodes a previously encoded numpy ndarray
+    with proper shape and dtype
+    :param dct: (dict) json encoded ndarray
+    :return: (ndarray) if input was an encoded ndarray
+    """
+    if isinstance(dct, dict) and '__ndarray__' in dct:
+        data = base64.b64decode(dct['__ndarray__'])
+        return np.frombuffer(data, dct['dtype']).reshape(dct['shape'])
+    return dct
+
+# Overload dump/load to default use this behavior.
+def dumps(*args, **kwargs):
+    kwargs.setdefault('cls', NumpyEncoder)
+    return json.dumps(*args, **kwargs)
+
+def loads(*args, **kwargs):
+    kwargs.setdefault('object_hook', json_numpy_obj_hook)    
+    return json.loads(*args, **kwargs)
+
+def dump(*args, **kwargs):
+    kwargs.setdefault('cls', NumpyEncoder)
+    return json.dump(*args, **kwargs)
+
+def load(*args, **kwargs):
+    kwargs.setdefault('object_hook', json_numpy_obj_hook)
+    return json.load(*args, **kwargs)
+
 
 class ObjectDetectionNode:
 
@@ -54,7 +88,7 @@ class ObjectDetectionNode:
             'box_classes' : classes.cpu().numpy(),
         }
         
-        self.data_pub.publish(json.dumps(msg, cls=NumpyEncoder))
+        self.data_pub.publish(dumps(msg, cls=NumpyEncoder))
         self.annotated_image_pub(self.cv_bridge.cv2_to_imgmsg(annotated_img))
 
 
